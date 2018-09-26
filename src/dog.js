2e0912cc5b797569b7de4dcf69887d8f03a29e0f
madtab7 <madtab7@gmail.com>
2018-09-26 10:43:52 -0400

    Done.

--- src/dog.js
+++ src/dog.js
@@ -1,3 +1,24 @@
 class Dog {
-  
+
+  constructor(dogJsonObj){
+    this.id = dogJsonObj.id;
+    this.name = dogJsonObj.name;
+    this.breed = dogJsonObj.breed;
+    this.sex = dogJsonObj.sex;
+    Dog.all.push(this)
+  }
+
+  render(){
+    return `
+    <tr>
+      <td>${this.name}</td>
+      <td>${this.breed}</td>
+      <td>${this.sex}</td>
+      <td><button type="button" id="edit-${this.id}">edit</button></td>
+    </tr>
+    `
+  }
+
 }
+
+Dog.all = [];
