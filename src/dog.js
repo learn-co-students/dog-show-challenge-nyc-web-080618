8c9fa4f6039ab689e2c44b110d5813d970626eeb
rendnil <andrew.lindner.15@gmail.com>
2018-09-26 13:15:04 -0400

    Done.

--- src/dog.js
+++ src/dog.js
@@ -1,3 +1,37 @@
+dog_store  = []
+
 class Dog {
-  
+
+  constructor(dogObj){
+    this.id = dogObj.id
+    this.name = dogObj.name
+    this.breed = dogObj.breed
+    this.sex = dogObj.sex
+    dog_store.push(this)
+  }
+
+
+  renderOneDog(){
+    return (`
+      <tr id=${this.id}>
+      <td>${this.name}</td>
+      <td>${this.breed}</td>
+      <td>${this.sex}</td>
+      <td><button class= "button">Edit</button></td>
+      </tr>
+      `)
+
+  }
+
+  static renderEditedDog(dogobj){
+    return(`
+      <td>${dogobj.name}</td>
+      <td>${dogobj.breed}</td>
+      <td>${dogobj.sex}</td>
+      <td><button class= "button">Edit</button></td>
+      `)
+  }
+
+
+
 }
