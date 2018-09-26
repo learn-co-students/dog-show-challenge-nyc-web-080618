9af133c0683224a4f13924211b28287c59f3d901
Richard Chen <richard.c.chen@gmail.com>
2018-09-26 15:17:43 -0400

    Done.

--- src/adapter.js
+++ src/adapter.js
@@ -1,3 +1,23 @@
 class Adapter {
-  
-}
+
+  static getDogs(){
+    return fetch("http://localhost:3000/dogs")
+    .then((resp)=> resp.json())
+  }
+
+  static editDog(Obj){
+    console.log(Obj);
+    return fetch(`http://localhost:3000/dogs/${Obj.id}`, {
+       method: 'PATCH',
+       headers: {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json'
+       },
+       body: JSON.stringify(Obj)
+    }).then(resp => resp.json())
+  }
+
+
+
+
+} //end of class
