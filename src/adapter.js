29b069c746b2efe5b8bfa585497d45a5b044be3a
github username <bobjin42@gmail.com>
2018-09-26 13:31:21 -0400

    Done.

--- src/adapter.js
+++ src/adapter.js
@@ -1,3 +1,19 @@
 class Adapter {
-  
+
+  static getDogs() {
+    return fetch('http://localhost:3000/dogs')
+      .then((respond) => respond.json())
+    }
+
+  static editDogs(dogObj, dogId) {
+    return fetch(`http://localhost:3000/dogs/${dogId}`, {
+      method: 'PATCH',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+      body: JSON.stringify(dogObj)
+    }).then(resp => resp.json())
+  }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const tablePlace = document.getElementById('table-body')
+
+
+Adapter.getDogs().then((el) => {
+  console.log(el)
+  tablePlace.innerHTML = el.map((dogObj) => {
+    let dog = new Dog(dogObj)
+      return dog.render();
+    }).join("")
+  })
+
 })
+
+function edit() {
+  const tablePlace = document.getElementById('table-body')
+  const editPlace = document.getElementById('dog-form')
+  const dogTarget = Dog.all.find((dog) => event.target.id == dog.id)
+  const targetId = dogTarget.id
+  editPlace.innerHTML = dogTarget.editForm()
+  editPlace.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const name = document.getElementById(`name-${targetId}`).value
+    const breed = document.getElementById(`breed-${targetId}`).value
+    const sex = document.getElementById(`sex-${targetId}`).value
+    Adapter.editDogs({name, breed, sex}, targetId)
+    .then(dogUpdate => {
+      let editName = document.getElementById(`edit-name-${targetId}`)
+      let editBreed = document.getElementById(`edit-breed-${targetId}`)
+      let editSex = document.getElementById(`edit-sex-${targetId}`)
+        editName.innerHTML = name
+        editBreed.innerHTML = breed
+        editSex.innerHTML = sex
+    })
+  })
+}
