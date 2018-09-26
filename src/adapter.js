7f4c90f6b78bc3b40fc9e39b1896245150820d5d
mccoleman75225 <michaelcoleman19@gmail.com>
2018-09-26 15:29:36 -0400

    Done.

--- src/adapter.js
+++ src/adapter.js
@@ -1,3 +1,40 @@
 class Adapter {
-  
+
+  // controller to get a list of each of the different
+  // dogs
+  static getDogs(){
+    return fetch('http://localhost:3000/dogs')
+      .then((resp) => resp.json())
+  }
+
+  // controller to add a new dog to the lsit of dogs
+  static createDog(dogObject){
+    return fetch('http://localhost:3000/dogs', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'content-Type': 'application/json'
+      },
+      body: JSON.stringify(dogObject)
+    }).then(resp => resp.json())
+  }
+
+  static editDog(dogObject){
+    return fetch(`http://localhost:3000/dogs/${dogObject.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Accept': 'application/json',
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        id: dogObject.id,
+        name: dogObject.name,
+        breed: dogObject.breed,
+        sex: dogObject.sex
+
+      })
+    }).then(resp => resp.json())
+  }
+
+
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const dogDisplayTable = document.getElementById("table-body")
+  const addANewDogbutton = document.getElementById("dog-form")
+
+
+
+
+  Adapter.getDogs()
+    .then(function(myJson) {
+      myJson.forEach(function(dog) {
+        let newDog = new Dog(dog.name, dog.breed, dog.sex, dog.id)
+        dogDisplayTable.innerHTML += newDog.render()
+      })
+    })
+
+  addANewDogbutton.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const addANewDogForm = document.getElementsByClassName("dogInformation")
+    const name = addANewDogForm.name.value
+    const breed = addANewDogForm.breed.value
+    const sex = addANewDogForm.sex.value
+    const id = event.target[3].id
+
+
+    // Adapter.createDog({name,breed,sex})
+    // .then(json => {
+    //   let dog = new Dog(json.name,json.breed,json.sex)
+    //   dogDisplayTable.innerHTML += dog.render()
+    //
+    // })
+
+    Adapter.editDog({name,breed,sex,id})
+
+
+  })
+
+dogDisplayTable.addEventListener("click", (event) => {
+  event.preventDefault()
+  if(event.target.className === "editButton"){
+    let dogId = event.target.id
+    let dog = Dog.findDog(dogId)
+    addANewDogbutton.innerHTML = `<form id='dog-form' tag="${dogId}" class="padding margin border-round border-grey">
+      <input type="name" name="name" placeholder="name" value="${dog.name}" class="dogInformation">
+      <input type="breed" name="breed" placeholder="breed" value="${dog.breed}" class="dogInformation">
+      <input type="sex" name="sex" placeholder="sex" value="${dog.sex}" class="dogInformation">
+      <input type="submit"value="Update this dog" id="${dogId}" >
+    </form>`
+
+    const editADogButton = document.getElementById("#editDogForm")
+
+    editADogButton.addEventListener("submit", (event) => {
+
+    })
+
+
+  }
+
+})
+
+
+
+
 })
