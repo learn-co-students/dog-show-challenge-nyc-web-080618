2614c15b45a59abf0ab4208fb7ac64d864f5733d
kristinatong <kristina.tong@gmail.com>
2018-09-26 15:51:23 -0400

    Done.

--- src/adapter.js
+++ src/adapter.js
@@ -1,3 +1,22 @@
 class Adapter {
-  
+  static getDogs(){
+    return fetch('http://localhost:3000/dogs')
+    .then(response => response.json())
+  }
+
+  static editDog(id,updatedDog){
+    return fetch(`http://localhost:3000/dogs/${id}`,{
+      method: 'PATCH',
+      headers: {
+        'Accept':'application/json',
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({
+        name: updatedDog.name,
+        breed: updatedDog.breed,
+        sex: updatedDog.sex,
+        id: updatedDog.id
+      })
+    })
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener('DOMContentLoaded', () => {
+const dogTable = document.getElementById('table-body')
+const dogForm = document.getElementById('dog-form')
 
+document.addEventListener('DOMContentLoaded', () => {
+  Adapter.getDogs().then(dogJson =>{
+    dogJson.forEach((dogObject)=>{
+      let dog = new Dog(dogObject)
+      dogTable.innerHTML += dog.render()
+    })
+  })
+})
+
+document.addEventListener('click',(event)=>{
+
+  if(event.target.tagName === 'BUTTON' && event.target.innerText === 'Edit'){
+    const dogObject = Dog.findDogById(parseInt(event.target.parentNode.parentNode.id))
+    dogForm.name.value = dogObject.name
+    dogForm.breed.value = dogObject.breed
+    dogForm.sex.value = dogObject.sex
+    dogForm.dataset.id = dogObject.id
+  }else if(event.target.tagName === 'INPUT'){
+    const dogId = parseInt(dogForm.dataset.id)
+    const dogInput = {
+      name: dogForm.name.value,
+      breed: dogForm.breed.value,
+      sex: dogForm.sex.value
+    }
+    Adapter.editDog(dogId,dogInput)
+    const dogRow = document.getElementById(dogId)
+    dogRow = dogObject.render()
+  }
 })
